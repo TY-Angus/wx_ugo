fcbeac453fc375a6f0a3f7287273c1d0ee983c57
angus <2936454740@qq.com>
2020-02-03 13:52:49 +0800

    02.商品详情页完成，以及添加到购物车功能，客服功能

--- src/mixins/goods_detail/main.js
+++ src/mixins/goods_detail/main.js
@@ -3,7 +3,8 @@ import wepy from 'wepy'
 export default class extends wepy.mixin {
   data = {
     goods_id: 0,
-    goodsInfo: {}
+    goodsInfo: {},
+    addressInfo: null
   }
   onLoad(options) {
     console.log(options)
@@ -19,5 +20,51 @@ export default class extends wepy.mixin {
 
     this.goodsInfo = data.message
     this.$apply()
+  }
+
+  methods = {
+    preview(current) {
+      wx.previewImage({
+        urls: this.goodsInfo.pics.map(item => item.pics_big),
+        // 当前默认预览图片
+        current
+      })
+    },
+
+    // 获取收货地址
+    async chooseAddress() {
+      const res = await wepy.chooseAddress().catch(err => err)
+      if (res.errMsg !== 'chooseAddress:ok') {
+        return wepy.baseToast('获取收货地址失败')
+      }
+      this.addressInfo = res
+      wepy.setStorageSync('address', res)
+      this.$apply()
+    },
+
+    // 加入购物车事件
+    addToCart() {
+      // 调用全局自定义的共享方法
+      this.$parent.addGoodsToCart(this.goodsInfo)
+
+      // 提示用户加入了购物车
+      wepy.showToast({
+        title: '已加入购物车',
+        icon: 'success'
+      })
+    }
+  }
+
+  // 计算属性节点
+  computed = {
+    addressStr() {
+      if (this.addressInfo === null) {
+        return '请选择收货地址'
+      }
+      const addr = this.addressInfo
+      const str =
+        addr.provinceName + addr.cityName + addr.countyName + addr.detailInfo
+      return str
+    }
   }
 }
